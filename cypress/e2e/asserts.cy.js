5c93f6df06093f7295e7a37e3da59c87aeb21b55
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-08-17 16:56:06 -0300

    assert update

--- cypress/e2e/asserts.cy.js
+++ cypress/e2e/asserts.cy.js
@@ -2,4 +2,11 @@ it('Equality', ()=>{
     const a = 1;
 
     expect(a).equal(1);
+    expect(a, 'Deveria ser 1').equal(1);
+    expect(a).to.be.equal(1);
+    expect('a').not.to.be.equal('b')
+})
+
+it('Truthy', () =>{
+    
 })
